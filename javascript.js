1a84f100827acd89c6ba27a8d4bc14999cb0ee7a
stefantanasa <liketanasa@gmail.com>
2021-11-11 15:24:55 +0000

    Create Teams Groups
    It creates unordered list of teams

--- javascript.js
+++ javascript.js
@@ -1,11 +1,9 @@
 let addedMemberList = [];
 let addMemberButton = document.getElementById("add-member-button");
 let inputMemberForm = document.getElementById("member-to-add");
-
+let waitingList = [];
+let teamsObject = {};
 //create new member element
-let newElement = document.createElement("li");
-newElement.innerText = "george";
-newElement.className = "list-group-item";
 
 //get member elsement node and append to list
 let getCreateMemberListNode = document.getElementById("added-members-to-list");
@@ -20,4 +18,30 @@ const addMember = function () {
   addedMemberList.push(inputMemberForm.value);
 };
 
-//testing log
+//Get team numbers and number of members added
+
+let membersNumber = addedMemberList.length;
+let getTeamNode = document.getElementById("teams-list");
+const howManyTeams = function () {
+  let numberOfTeams = document.getElementById("input-numbers").value;
+  for (i = 0; i < numberOfTeams; i++) {
+    console.log(i);
+    let newTeam = document.createElement("li");
+    newTeam.innerText = `Team ${i + 1}`;
+    newTeam.className = "list-group-item";
+    getTeamNode.appendChild(newTeam);
+  }
+};
+
+const randomIndex = function () {};
+//test
+let teams = 3;
+let memList = ["george", "Stefan", "Anusha", "Amir", "seb", "Dan", "Daniela"];
+let obj = {};
+for (i = 0; i < 4; i++) {
+  for (x = 0; x < 4; x++) {
+    obj.x = memList[i];
+  }
+}
+
+console.log(obj);
