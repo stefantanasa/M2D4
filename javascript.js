a3e0ba270cd2a01294198b940871de12713af791
stefantanasa <liketanasa@gmail.com>
2021-11-11 20:39:58 +0000

    Split algoritm discovered
    Needs to be implemented in the function and then the fake list to be changed to the list created by the user

--- javascript.js
+++ javascript.js
@@ -1,4 +1,21 @@
 let addedMemberList = [];
+
+//fake list
+let memList = [
+  "George",
+  "Johny",
+  "Stefan",
+  "Anusha",
+  "Amir",
+  "Seb",
+  "Dan",
+  "Daniela",
+  "Nic",
+];
+//shuffle fake list
+let shuffled = memList.sort(() => Math.random() - 0.5);
+console.log(memList);
+
 let addMemberButton = document.getElementById("add-member-button");
 let inputMemberForm = document.getElementById("member-to-add");
 let waitingList = [];
@@ -16,32 +33,118 @@ const addMember = function () {
   newElement.className = "list-group-item";
   getCreateMemberListNode.appendChild(newElement);
   addedMemberList.push(inputMemberForm.value);
+  inputMemberForm.value = "";
 };
 
 //Get team numbers and number of members added
+let objTeams = {};
 
 let membersNumber = addedMemberList.length;
 let getTeamNode = document.getElementById("teams-list");
+let numberOfTeams = document.getElementById("input-numbers");
+//how many teams function
 const howManyTeams = function () {
-  let numberOfTeams = document.getElementById("input-numbers").value;
-  for (i = 0; i < numberOfTeams; i++) {
-    console.log(i);
+  for (i = 0; i < numberOfTeams.value; i++) {
     let newTeam = document.createElement("li");
     newTeam.innerText = `Team ${i + 1}`;
     newTeam.className = "list-group-item";
     getTeamNode.appendChild(newTeam);
   }
-};
 
-const randomIndex = function () {};
-//test
-let teams = 3;
-let memList = ["george", "Stefan", "Anusha", "Amir", "seb", "Dan", "Daniela"];
-let obj = {};
-for (i = 0; i < 4; i++) {
-  for (x = 0; x < 4; x++) {
-    obj.x = memList[i];
+  numberOfTeams.value = "";
+  console.log();
+
+  ////so far so good :)
+  for (i = 0; i < addedMemberList.length; i++) {
+    for (member = 0; member < addedMemberList.length; member++) {
+      objTeams[member + 1] = addedMemberList[member];
+    }
   }
+};
+// let obj = {};
+let chunk_size = 2;
+var index = 0;
+var arrayLength = memList.length;
+var tempArray = [];
+
+for (index = 0; index < arrayLength; index += chunk_size) {
+  myChunk = memList.slice(index, index + chunk_size);
+  // Do something if you want with the group
+  tempArray.push(myChunk);
 }
 
-console.log(obj);
+console.log(tempArray);
+
+for (i = 0; i < tempArray.length; i++) {
+  let oneElement = document.createElement("li");
+  oneElement.innerText = `This is ${tempArray[i]}`;
+  getCreateMemberListNode.appendChild(oneElement);
+}
+
+/*
+ * Returns an array with arrays of the given size.
+ *
+ * @param myArray {Array} array to split
+ * @param chunk_size {Integer} Size of every group
+ 
+
+
+
+function chunkArray(myArray, chunk_size) {
+  var index = 0;
+  var arrayLength = myArray.length;
+  var tempArray = [];
+
+  for (index = 0; index < arrayLength; index += chunk_size) {
+    myChunk = myArray.slice(index, index + chunk_size);
+    // Do something if you want with the group
+    tempArray.push(myChunk);
+  }
+
+  return tempArray;
+}
+// Split in group of 3 items
+var result = chunkArray(memList, Math.floor(memList.length / 4));
+// Outputs : [ [1,2,3] , [4,5,6] ,[7,8] ]
+
+for (let list = 0; list < result.length; list++) {
+  console.log(result[list]);
+}
+
+*/
+
+/* Object way
+let equalTeams = memList.length - (memList.length % 3);
+for (i = 0; i < equalTeams; i++) {
+  for (member = 0; member < equalTeams; member++) {
+    objTeams[member + 1] = memList[member];
+  }
+}
+console.log(objTeams);
+console.log(objTeams);
+
+for (i = 1; i < Object.keys(objTeams).length; i++) {
+  let oneElement = document.createElement("li");
+  oneElement.innerText = `This is ${objTeams[i]}`;
+  getCreateMemberListNode.appendChild(oneElement);
+}
+*/
+// for (i = 0; i < addedMemberList.length; i++) {
+//   for (x = 0; x < addedMemberList.length; x++) {
+//     obj[x + 1] = addedMemberList[x];
+//   }
+// }
+
+// const randomIndex = function () {};
+// //test
+
+// console.log(obj);
+
+// Shuffle the list
+
+// let shuffled = memList.sort(() => Math.random() - 0.5);
+// console.log(memList);
+
+// let equalTeams = memList.length - (memList.length % 3);
+
+// console.log(equalTeams);
